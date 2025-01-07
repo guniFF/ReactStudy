ad549b3be1942d810e329433517e5435734bad83
unknown <sky4tony@naver.com>
2025-01-08 06:05:04 +0900

    2025-01-08

---
+++ practice/my-app/src/chapter_03/Library.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Book from "./Book";
+
+function Library(props) {
+    return (
+        <div>
+            <Book name ="처음 만난 파이썬" numOfPage={300}/>
+            <Book name = ""/>
+        </div>
+    );
+}

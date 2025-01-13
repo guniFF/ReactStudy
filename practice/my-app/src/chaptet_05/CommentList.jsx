be25028d3738b699e590d4eb067a4fa1d2d833db
unknown <sky4tony@naver.com>
2025-01-14 02:35:02 +0900

    2025-01-13

--- practice/my-app/src/chaptet_05/CommentList.jsx
+++ practice/my-app/src/chaptet_05/CommentList.jsx
@@ -2,10 +2,32 @@
 
 import Comment from "./Comment";
 
+const comment_list = [
+    {
+        name : "김동건",
+        comment : "안녕하세요, 위젯입니다."
+    },
+    {
+        name : "유재석",
+        comment : "안녕하세요, 위젯입니다."
+    },
+    {
+        name : "강민경",
+        comment : "안녕하세요, 위젯입니다."
+    }
+]
+
 function CommentList(){
     return (
+        // <div>
+        //     <Comment name = {"김동건"} comment = {"안녕하세요, 위젯입니다."}/>
+        // </div>
         <div>
-            <Comment />
+            {comment_list.map((comment, idx)=>{
+                return (
+                    <Comment name = {comment.name} comment = {comment.comment} key ={idx}/>
+                )
+            })}
         </div>
     )
 }
